f80e99c74ea16b41beb4dfb496d55dbc3890797d
Karthick47v2 <karthicksharma11@gmail.com>
2022-05-08 12:05:42 +0530

    Refactor folders

--- test.py
+++ test.py
@@ -4,21 +4,28 @@ import cv2
 # numpy array
 import numpy as np
 
-# import model
-import tensorflow as tf
-from tensorflow import keras
+# # import model
+# import tensorflow as tf
+# from tensorflow import keras
 
-# sound recording
-import pyaudio
+# # sound recording
+# import pyaudio
 
-# save wav file
-import wave
+# # save wav file
+# import wave
 
-# run inference and sound rec as separate processes - inference may breakdown sound
-import multiprocessing as mp
+# # run inference and sound rec as separate processes - inference may breakdown sound
+# import multiprocessing as mp
 
-# debug 
-import time
+# # debug
+# import time
+
+from pathlib import Path
+
+base_path = Path(__file__).parent
+haar_cascade_path = str(
+    base_path / 'haarcascade_xml/haarcascade_frontalface_alt2.xml')
+
 
 img_size = 96
 edge_offset = 75
@@ -27,57 +34,59 @@ sample_rate = 44100
 frames_per_buffer = 1024
 no_of_channels = 1
 
-model = keras.models.load_model('./model')
+# model = keras.models.load_model('./model')
 
 
-def draw_landmark(frame):
-    landmarks = model.predict(frame.reshape(1, img_size, img_size, 1))
+# def draw_landmark(frame):
+#     landmarks = model.predict(frame.reshape(1, img_size, img_size, 1))
 
-    x = np.reshape(landmarks[:, :, :, np.arange(0, 136, 2)], (68)) * img_size
-    y = np.reshape(landmarks[:, :, :, np.arange(1, 136, 2)], (68)) * img_size
+#     x = np.reshape(landmarks[:, :, :, np.arange(0, 136, 2)], (68)) * img_size
+#     y = np.reshape(landmarks[:, :, :, np.arange(1, 136, 2)], (68)) * img_size
 
-    for x_coord, y_coord in zip(x, y):
-        cv2.circle(frame, tuple((int(x_coord), int(y_coord))),
-                   radius=1, color=(255, 0, 0), thickness=1)
+#     for x_coord, y_coord in zip(x, y):
+#         cv2.circle(frame, tuple((int(x_coord), int(y_coord))),
+#                    radius=1, color=(255, 0, 0), thickness=1)
 
-    return frame
+#     return frame
 
 
-def audio_func():
-    # initialize PyAudio
-    print("running on sub")
-    audio = pyaudio.PyAudio()
-    # https://people.csail.mit.edu/hubert/pyaudio/docs/
-    # formant, no of channels, sample rate were chose to match with Google Speech to Text input with reduction in size
-    stream = audio.open(format=pyaudio.paInt16, channels=no_of_channels,
-                        rate=sample_rate, frames_per_buffer=frames_per_buffer, input=True)
+# def audio_func():
+#     # initialize PyAudio
+#     print("running on sub")
+#     audio = pyaudio.PyAudio()
+#     # https://people.csail.mit.edu/hubert/pyaudio/docs/
+#     # formant, no of channels, sample rate were chose to match with Google Speech to Text input with reduction in size
+#     stream = audio.open(format=pyaudio.paInt16, channels=no_of_channels,
+#                         rate=sample_rate, frames_per_buffer=frames_per_buffer, input=True)
 
-    audio_frames = []
-    i = 0
-    while i < 1000:
-        print(i)
-        i += 1
-        audio_frames.append(stream.read(frames_per_buffer))
+#     audio_frames = []
+#     i = 0
+#     while i < 1000:
+#         print(i)
+#         i += 1
+#         audio_frames.append(stream.read(frames_per_buffer))
 
-    # stop recording and release resources
-    stream.stop_stream()
-    stream.close()
-    audio.terminate()
+#     # stop recording and release resources
+#     stream.stop_stream()
+#     stream.close()
+#     audio.terminate()
 
-    # save audio in required format
-    sound_file = wave.open("rec.wav", "wb")
-    sound_file.setnchannels(no_of_channels)
-    sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
-    sound_file.setframerate(sample_rate)
-    # join all frames inorder to create wav
-    sound_file.writeframes(b''.join(audio_frames))
-    sound_file.close()
+#     # save audio in required format
+#     sound_file = wave.open("rec.wav", "wb")
+#     sound_file.setnchannels(no_of_channels)
+#     sound_file.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+#     sound_file.setframerate(sample_rate)
+#     # join all frames inorder to create wav
+#     sound_file.writeframes(b''.join(audio_frames))
+#     sound_file.close()
 
 
 def video_func():
     # download prebuilt haarcascade xml for face detection
-    face_cascade = cv2.CascadeClassifier(
-        'haarcascade_xml/haarcascade_frontalface_alt2.xml')
+    # face_cascade = cv2.CascadeClassifier(
+    #     'haarcascade_xml/haarcascade_frontalface_alt2.xml')
+
+    face_cascade = cv2.CascadeClassifier(haar_cascade_path)
 
     # webcam
     cap = cv2.VideoCapture(0)
@@ -131,7 +140,7 @@ def video_func():
                 input_frame, (img_size, img_size)).astype(np.uint8)
 
             # inference
-            frame = draw_landmark(input_frame)
+            # frame = draw_landmark(input_frame)
             # print(model.predict(input_frame).shape)
 
         except:
@@ -167,8 +176,8 @@ def video_func():
 
 
 if __name__ == '__main__':
-    audio_cpu = mp.Process(target=audio_func)
-    audio_cpu.start()
+    # audio_cpu = mp.Process(target=audio_func)
+    # audio_cpu.start()
     video_func()
 
 
