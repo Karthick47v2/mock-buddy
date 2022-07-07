1d88169808808ad4c776b8a0cd4ffdababd8e4a2
Karthick47v2 <Karthicksharma11@gmail.com>
2022-07-07 22:58:41 +0530

    Delete model

--- react-app/mock-buddy/app/app.py
+++ react-app/mock-buddy/app/app.py
@@ -7,7 +7,7 @@ from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 
 from src.face_model import FaceModel
-from src.audio_util import Audio
+from src.audio_util import AudioUtil
 
 
 #### SESSION ######
@@ -29,22 +29,30 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './secrets/mock-buddy.json'
 # initialize models
 fm = FaceModel()
 
-
-# # generate frames
-# def generate_frame(cam):
-#     # audio_cpu = Process(target=gen_audio_frame, args=(
-#     #     run_rec, (base_path / 'audio_out')))
-#     # audio_cpu.start()
-
-#     while run_rec.value:
-#         frame = cam.generate_frame()
-#         # return multiple times / kinda generator - jpeg format for cv frame
-#         yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
-#     # audio_cpu.join()
-
-
 # Restful APIs
+
+
+@app.route('/init/', methods=['GET'])
+def init_model():
+    """initialize or rest var
+
+    Returns:
+        dict[str,str]: response
+    """
+    fm.reset()
+    return {'status': 200}
+
+
+@app.route('/vid_fb/', methods=['GET'])
+def get_vid_fb():
+    """return feedback
+
+    Returns:
+        dict[str,str]: response
+    """
+    return fm.get_vid_feedback()
+
+
 @app.route('/audio_out/', methods=['POST'])
 def get_audio():
     """Process audio file sent from client
@@ -52,35 +60,32 @@ def get_audio():
     Returns:
         dict[str,str]: response
     """
-    # try:
-    file = request.files['file']
-    # name as M_D_Y_H_M_S format in order to avoid overwriting issue
-    audio_file = Audio(file_name=datetime.datetime.now().strftime(
-        '%m_%d_%Y_%H_%M_%S') + '.wav')
+    try:
+        file = request.files['file']
+        # name as M_D_Y_H_M_S format in order to avoid overwriting issue
+        audio_file = AudioUtil(file_name=datetime.datetime.now().strftime(
+            '%m_%d_%Y_%H_%M_%S') + '.wav')
 
-    audio_duration = audio_file.change_audio_format(
-        file)  # AUDIO DUCRATION NEEDED ####################################
+        audio_file.change_audio_format(file)
 
-    print(audio_duration)
+        # upload audio to ggl storage (mandatory for transcribing long audio (> 1mins))
+        audio_file.storage.upload_to_bucket(audio_file.file_name)
 
-    # upload audio to ggl storage (mandatory for transcribing long audio (> 1mins))
-    audio_file.storage.upload_to_bucket(audio_file.file_name)
+        speech_rate = audio_file.get_speech_rate()
 
-    audio_file.stt.transcribe(
-        audio_file.storage.bucket_name, audio_file.file_name)
+        # delete existing file in storage (local and cloud)
+        audio_file.storage.delete_file(audio_file.file_name)
+        if os.path.exists(audio_file.file_name):
+            os.remove(audio_file.file_name)
 
-    # delete existing file in storage (local and cloud)
-    audio_file.storage.delete_file(audio_file.file_name)
-    if os.path.exists(audio_file.file_name):
-        os.remove(audio_file.file_name)
-
-    return {
-        'audio': 'received'
-    }
-    # except:
-    # return{
-    # 'audio': 'failed'
-    # }
+        return {
+            'status': 200,
+            'wpm': speech_rate
+        }
+    except:
+        return{
+            'status': 400
+        }
 
 
 # SocketIO events
@@ -92,7 +97,6 @@ def detect_facial_points(uri):
         uri (str): base64 encoded frame
     """
     fm.detect_face(uri)
-    emit('get_output', request.sid, to=request.sid)
 
 
 @socketio.on('connect')
@@ -109,8 +113,6 @@ def disconnected():
 
 # main thread
 if __name__ == '__main__':
-    # create shared var
-    # run_rec = Manager().Value("b", True)
 
     # currently using eventlet server for production env
     # socketio will also take care of restful api calls
