bb49af3cf5495a30825bd03cfe98e5b0f09d5ab5
Karthick47v2 <Karthicksharma11@gmail.com>
2022-07-10 21:49:00 +0530

    Add slide input modal

---
+++ react-app/mock-buddy/src/Components/modalPopup.jsx
@@ -0,0 +1,75 @@
+import React, { forwardRef, useState } from "react";
+import { Modal } from "react-bootstrap";
+import { SlideInput } from "./slideInput";
+
+/**
+ * JSX component for displaying popup modal
+ * @param {Object} props - component props
+ * @param {() => void} props.handleClose - close modal
+ * @param {(gLink: string) => void} props.setGLink - set glink value
+ * @param {Ref} ref - reference to container object
+ * @returns {JSX.Element} - popup modal
+ */
+export const ModalPopup = forwardRef((props, ref) => {
+  /**
+   * @type {[Boolean, Function]} Error
+   */
+  const [error, setError] = useState(false);
+  /**
+   * @type {[string, Function]}
+   */
+  const [tempGLink, setTempGLink] = useState("");
+
+  const handleFormChange = (e) => {
+    setTempGLink(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+
+    if (
+      form.checkValidity() === false ||
+      !tempGLink.includes("docs.google.com/presentation/d/")
+    ) {
+      setError(true);
+      e.stopPropagation();
+    } else {
+      setError(false);
+      props.setGLink(tempGLink);
+      props.handleClose();
+    }
+  };
+
+  return (
+    <Modal
+      show={props.show}
+      onHide={props.handleClose}
+      centered
+      backdrop="static"
+      container={ref}
+      style={{ position: "absolute", backgroundColor: "black" }}
+      className="text-light"
+    >
+      <Modal.Header className="d-flex justify-content-center align-items-center">
+        <Modal.Title>Paste Google slide link</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h5>
+          You need a publicly shared google slide in order to continue
+          'Presentation mode' or else, switch to 'Speech mode'
+        </h5>
+        <SlideInput
+          onFormSubmit={handleFormSubmit}
+          onFormChange={handleFormChange}
+          formError={error}
+        />
+      </Modal.Body>
+      <Modal.Footer className="d-flex justify-content-center align-items-center">
+        {/* <Button variant="primary" onClick={handleClose}>
+          Continue
+        </Button> */}
+      </Modal.Footer>
+    </Modal>
+  );
+});
---
+++ react-app/mock-buddy/src/Components/slideInput.jsx
@@ -0,0 +1,37 @@
+import { Button, Form, FloatingLabel } from "react-bootstrap";
+
+/**
+ * JSX component for modal user input
+ * @param {Object} props - component props
+ * @param {() => void} props.onFormSubmit - submit form
+ * @param {() => void} props.onFormChange - handle user input event
+ * @param {Boolean} props.formError - error status
+ * @returns {JSX.Element} - form
+ */
+export const SlideInput = (props) => {
+  return (
+    <Form noValidate onSubmit={props.onFormSubmit}>
+      <Form.Group className="mb-3">
+        <FloatingLabel
+          controlId="glink"
+          label="Paste link here"
+          className="text-dark"
+        >
+          <Form.Control
+            required
+            type="text"
+            placeholder=""
+            isInvalid={props.formError}
+            onChange={props.onFormChange}
+          />
+          <Form.Control.Feedback type="invalid">
+            Please enter a valid URL
+          </Form.Control.Feedback>
+        </FloatingLabel>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Continue
+      </Button>
+    </Form>
+  );
+};
