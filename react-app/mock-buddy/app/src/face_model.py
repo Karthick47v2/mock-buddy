47d18b80d7fcc7f64059132df2d5f0f53d7c89da
Karthick47v2 <Karthicksharma11@gmail.com>
2022-07-13 22:28:08 +0530

    fix pylint error

--- react-app/mock-buddy/app/src/face_model.py
+++ react-app/mock-buddy/app/src/face_model.py
@@ -1,11 +1,11 @@
 """face and facial features detection"""
 
+# pylint: disable=E0611
+
 import base64
 import numpy as np
 from cv2 import cv2
 from tensorflow import keras
-
-# pylint: disable=no-self-use
 
 
 class FaceModel:
@@ -17,42 +17,76 @@ class FaceModel:
         self.__face_dnn = cv2.dnn.readNetFromCaffe(
             'dnn/deploy.prototxt', 'dnn/res10_300x300_ssd_iter_140000.caffemodel')
         self.__model = keras.models.load_model('./conv_model')
+        self.__interactivity = None
+        self.__inactive = None
+        self.__look = None
 
-    def __decode_b64(self, uri):
+    @classmethod
+    def __decode_b64(cls, uri):
         """Decode base64 to cv2 frame
 
         Args:
             uri (str): base64 encoded frame
 
         Returns:
-            img array: generated img array from buffer
+            ndarray: generated img array from buffer
         """
         b64_str = uri.split(',')[1]
         buff = np.frombuffer(base64.b64decode(b64_str), np.int8)
         return cv2.imdecode(buff, cv2.IMREAD_COLOR)
 
     def reset(self):
+        """ Reset user scores to default
+        """
         self.__interactivity = [0]
         self.__inactive = 0
         self.__look = 0
 
     def __extract_kps(self, frame):
+        """ Extract keypoints from input image using Facial landmark detection model
+
+        Args:
+            frame (ndarray): image array
+
+        Returns:
+            ndarray: numpy array of keypoints
+        """
         y_pred = self.__model.predict(np.expand_dims(
             frame.reshape(FaceModel.__IMG_SIZE, FaceModel.__IMG_SIZE, 1), 0))
         return self.__get_vals(y_pred[0])
 
-    def __get_vals(self, kps, threshold=17):
+    @classmethod
+    def __get_vals(cls, kps, threshold=17):
+        """ Return interactivity and posture scores
+
+        Args:
+            kps (ndarray): numpy array of keypoints
+            threshold (int, optional): threshold for face turning freedom. Defaults to 17.
+
+        Returns:
+            Tuple(float, float): posture and interactivity score
+        """
         y_axis = kps[54:62:2].mean()
         interactivity = abs(y_axis - (FaceModel.__IMG_SIZE / 2))
 
         abs_l = np.abs(kps[0:16:2] - y_axis).mean()
         abs_r = np.abs(kps[18:34:2] - y_axis).mean()
 
-        ln = abs(abs_l - abs_r)
+        abs_len = abs(abs_l - abs_r)
 
-        return ln < threshold, interactivity
+        return abs_len < threshold, interactivity
 
-    def __resize_w_pad(self, img, req_size):
+    @classmethod
+    def __resize_w_pad(cls, img, req_size):
+        """ Resize and pad input image inorder to, not to change aspect ration of image
+
+        Args:
+            img (ndarray): image array
+            req_size (Tuple(int, int)): tuple of expected image size
+
+        Returns:
+           ndarray: resized image array
+        """
         original_shape = (img.shape[1], img.shape[0])
 
         ratio = float(max(req_size))/max(original_shape)
@@ -96,17 +130,23 @@ class FaceModel:
                 frame = self.__resize_w_pad(
                     frame, (FaceModel.__IMG_SIZE, FaceModel.__IMG_SIZE)).astype(np.uint8)
 
-                ln, interac = self.__extract_kps(frame)
+                abs_len, interac = self.__extract_kps(frame)
 
                 self.__interactivity.append(interac)
-                self.__look += int(ln)
+                self.__look += int(abs_len)
                 return
 
         self.__inactive += 1
 
     def get_vid_feedback(self):
+        """ Return all processed scores
+
+        Returns:
+            Dict(float, float, float): visibility, posture and interactivity scores
+        """
         return {
-            'visibility_score': (len(self.__interactivity)) / float(self.__inactive + len(self.__interactivity)),
+            'visibility_score': (len(self.__interactivity)) / float(self.__inactive +
+                                                                    len(self.__interactivity)),
             'posture_score': self.__look / len(self.__interactivity),
             'interactivity_score': sum(self.__interactivity) / len(self.__interactivity)
         }
