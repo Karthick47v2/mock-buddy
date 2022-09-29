db8992f458d370085c4b0a8512b9291ec2dc648b
Karthick47v2 <Karthicksharma11@gmail.com>
2022-09-29 22:57:18 +0530

    render mistakes

---
+++ src/Components/mistakesAccordionMapper.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Accordion } from "react-bootstrap";
+
+export const MistakesAccordionMapper = (props) => {
+  return (
+    <Accordion className="mt-5" data-testid="accordion">
+      {props.mistakes.map((slide, idx) => (
+        <Accordion.Item key={idx} eventKey={idx} className="mt-1">
+          <Accordion.Header>{`Slide ${slide[0].id}`}</Accordion.Header>
+          <Accordion.Body>
+            {slide.map((mistake, idx) => (
+              <div key={idx}>
+                <ul>
+                  <li style={{ color: "#e69b00" }}>{`#${idx + 1} - ${
+                    mistake.exp
+                  }`}</li>
+                </ul>
+                <p
+                  style={{ color: "#aa070d" }}
+                >{`Error: ${mistake.mistake}`}</p>
+                <p>{`Suggestion(s): ${mistake.top3}`}</p>
+              </div>
+            ))}
+          </Accordion.Body>
+        </Accordion.Item>
+      ))}
+    </Accordion>
+  );
+};
